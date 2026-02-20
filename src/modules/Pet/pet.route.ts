80046401f7f1bad521fbddcffc13dae681ab1ce0
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-20 19:37:53 +0800

    create api for login

--- src/modules/Pet/pet.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { PetController } from "./pet.controller";
-import auth, { UserRole } from "../../middlewares/auth";
-
-const router = express.Router();
-
-router.post("/", auth(UserRole.owner), PetController.createPets);
-
-export const PetRoutes = router;
